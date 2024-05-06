a0917447af6a1a39731044822ae89c67291534b8
ilkham <garaevilham184@gmail.com>
2024-05-06 10:59:57 +0300

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/laba6
+# Build directory: C:/laba6/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
